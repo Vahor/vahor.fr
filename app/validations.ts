207390810ca1a5ca97239603f3cb653d43de4a34
Vahor <nathan.d0601@gmail.com>
2021-09-18 17:46:20 +0200

    fix schema

--- app/validations.ts
+++ app/validations.ts
@@ -3,6 +3,6 @@ import { z } from "zod"
 export const ContactMessage = z.object({
     name: z.string(),
     subject: z.string().max(60),
-    text: z.string().max(600),
+    html: z.string().max(600),
     from: z.string().email(),
 })
