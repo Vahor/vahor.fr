036ecedb1034c485805f78977ada77e1a24be274
Vahor <nathan.d0601@gmail.com>
2021-09-18 18:31:54 +0200

    Ajout images.unsplash.com pour les tests images

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   images: {
     domains: [
       "static.vahor.fr", // Notion cover and logo self host
-      "www.notion.so", "s3.us-west-2.amazonaws.com" // Notion cover
+      "www.notion.so", "images.unsplash.com", "s3.us-west-2.amazonaws.com" // Notion cover
     ],
   },
   optimization: {
