1f44b33bf7578e4380a5b3de5799f2e31196cad0
Vahor <nathan.d0601@gmail.com>
2021-09-19 18:24:58 +0200

    Retrait test domains

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
   images: {
     domains: [
       "static.vahor.fr", // Notion cover and logo self host
-      "www.notion.so", "images.unsplash.com", "s3.us-west-2.amazonaws.com" // Notion cover
     ],
   },
   optimization: {
