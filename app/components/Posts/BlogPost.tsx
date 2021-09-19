ba50a4e3bf54e8a4937d27caa33b51dd90526ea6
Vahor <nathan.d0601@gmail.com>
2021-09-19 19:29:41 +0200

    Ajout de la date de publication dans la liste des posts

--- app/components/Posts/BlogPost.tsx
+++ app/components/Posts/BlogPost.tsx
@@ -1,5 +1,7 @@
 import { NotionPage } from "types"
 import Link from 'next/link';
+import dayjs from "dayjs";
+import 'dayjs/locale/fr' // load on demand
 
 
 interface Props {
@@ -12,9 +14,14 @@ const BlogPost = ({ post }: Props) => {
             <a className="!no-underline">
                 <div className="pb-8 w-full">
                     <div className="flex flex-col">
-                        <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
-                            {post.properties.Name.title[0].plain_text}
-                        </h4>
+                        <div className="flex justify-between items-center">
+                            <h4 className="!m-0 text-lg md:text-xl font-medium mb-2 text-gray-900 dark:text-gray-100">
+                                {post.properties.Name.title[0].plain_text}
+                            </h4>
+                            <span className="font-light">
+                                Publié le {dayjs(post.properties.Date.date.start).locale("fr").format("DD MMMM YYYY")}
+                            </span>
+                        </div>
                         <p className="text-gray-600 dark:text-gray-400">
                             {post.properties.Summary?.rich_text[0].plain_text}
                         </p>
