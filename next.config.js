fe0724a0653527635ed1b7db0c23f9224ec7e39b
Vahor <nathan.d0601@gmail.com>
2021-09-19 19:06:46 +0200

    Ajout du cache pour les fonts / images locales

--- next.config.js
+++ next.config.js
@@ -8,5 +8,28 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/fonts/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=2678400, s-maxage=2678400, stale-while-revalidate=2678400',
+          },
+        ],
+      },
+      {
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2678400, s-maxage=2678400, stale-while-revalidate=2678400',
+          },
+        ],
+      },
+    ]
+  },
 }
