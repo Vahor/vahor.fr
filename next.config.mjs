b80a4bac3a6dc19ad7321ce94efcc617493eb95d
Vahor <me@vahor.fr>
2024-04-27 21:47:16 +0200

    add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,41 @@
 import { withContentlayer } from "next-contentlayer2";
 
+const securityHeaders = [
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "SAMEORIGIN",
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-XSS-Protection",
+		value: "1; mode=block",
+	},
+	{
+		key: "Referrer-Policy",
+		value: "same-origin",
+	},
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+
+	headers: async () => {
+		return [
+			{
+				source: "/(.*)",
+				headers: securityHeaders,
+			},
+		];
+	},
 
 	env: {
 		BUILD_TIME: new Date().toISOString(),
