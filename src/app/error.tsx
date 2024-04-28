2a8a8a23941071224e62c3642eb99c84b1748ab7
Vahor <me@vahor.fr>
2024-04-28 17:24:55 +0200

    add 500

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,22 +1,36 @@
+"use client";
+
+import A from "@/components/A";
 import { TriggerSearch } from "@/components/TriggerSearch";
 import { Button } from "@/components/ui/button";
+import { GITHUB_REPOSITORY } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-import whereAmI from "../../public/where-am-i.gif";
+import homerError from "../../public/error.gif";
 
-export default function NotFound() {
+export default function ErrorPage() {
 	return (
 		<div className="sm:h-full w-full flex flex-col lg:flex-row gap-8 md:gap-12 items-center sm:justify-center md:mt-[-105px] container">
-			<Image src={whereAmI} alt="Where am I?" className="rounded-md shadow" unoptimized />
+			<Image
+				src={homerError}
+				alt="Homer Simpson error"
+				className="rounded-md shadow"
+				unoptimized
+			/>
 			<main className="flex flex-col gap-4">
 				<h1 className="font-semibold text-6xl text-black dark:text-white">
-					Oops !
+					Erreur!
 				</h1>
 				<div className="text-muted-foreground">
-					<p>La page que vous cherchez n'existe pas.</p>
-					<p>
-						Peut-être que vous avez mal tapé l'adresse ou que la page a été
-						supprimée.
+					<p>Une erreur s'est produite.</p>
+					<p>Ce n'est pas de votre faute, c'est de la mienne.</p>
+					<p className="mt-2">
+						Si vous voulez m'aider à la corriger, le repository est disponible
+						sur <A href={GITHUB_REPOSITORY}>GitHub</A>.
+					</p>
+					<p className="mt-2">
+						Et si vous voulez simplement retourner à l'accueil, utilisez les
+						boutons ci-dessous.
 					</p>
 				</div>
 				<div className="space-x-2">
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,7 @@ const PROTOCOL = env.NEXT_PUBLIC_VERCEL_URL.includes("localhost")
 	: "https";
 export const BASE_URL = `${PROTOCOL}://${env.NEXT_PUBLIC_VERCEL_URL}`;
 export const GITHUB_PROFILE = "https://github.com/vahor";
+export const GITHUB_REPOSITORY = `${GITHUB_PROFILE}/vahor.fr`;
 export const TWITTER_PROFILE = "https://twitter.com/vahor_";
 export const LINKEDIN_PROFILE = "https://www.linkedin.com/in/nathan--david/";
 export const EMAIL = "me@vahor.fr";
