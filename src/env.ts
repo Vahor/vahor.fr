66679aac8d2dcbad0abb344b44f7a66af0e339dd
Vahor <me@vahor.fr>
2024-05-01 21:40:20 +0200

    fix: force vahor.fr hostname in production

--- src/env.ts
+++ src/env.ts
@@ -13,14 +13,17 @@ export const env = createEnv({
 
 	client: {
 		NEXT_PUBLIC_VERCEL_URL: z.string(),
+		NEXT_PUBLIC_VERCEL_ENV: z.string(),
 		NEXT_PUBLIC_BUILD_TIME: z.coerce.string(),
 	},
 
 	runtimeEnv: {
 		NODE_ENV: process.env.NODE_ENV,
 		BUILD_TIME: process.env.BUILD_TIME,
+
 		NEXT_PUBLIC_BUILD_TIME: process.env.BUILD_TIME,
 		NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+		NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
 
 		SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
 		SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
