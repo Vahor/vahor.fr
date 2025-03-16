f768c36f6f1c3bf5aad48be8d91393984cee69d4
Vahor <me@vahor.fr>
2025-03-16 11:41:19 +0100

    add security headers
    c

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,39 @@
 import type { NextConfig } from "next";
 import { withContentlayer } from "next-contentlayer2";
 
+const securityHeaders = [
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+	{
+		key: "Referrer-Policy",
+		value: "strict-origin-when-cross-origin",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+	{
+		key: "X-Frame-Options",
+		value: "DENY",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=31536000; includeSubDomains",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=()",
+	},
+];
+
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	poweredByHeader: false,
@@ -14,6 +47,15 @@ const nextConfig: NextConfig = {
 				pathname: "/image/*",
 			},
 		],
+	},
+
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: securityHeaders,
+			},
+		];
 	},
 
 	env: {
