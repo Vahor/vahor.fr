5650bdc1a6775d5023e154cc3cf971e6beb57141
Nathan <nathan.d0601@gmail.com>
2025-04-10 23:19:00 +0200

    add llms-txt tool (#187)
    * add llms-txt tool
    
    * add domain and .md extension
    
    * replace public in md files
    
    * add datePublished in frontMatter
    
    * remove unnecssary return

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-bash scripts/copy-markdown.sh
-bash scripts/llms.txt.sh
---
+++ scripts/generate-llms.txt.ts
@@ -0,0 +1,61 @@
+import {
+	LLMS_TXT_OUTPUT_DIR_INPUT,
+	type PluginOptions,
+	generate,
+} from "@vahor/llms-txt";
+import { allDocuments } from "contentlayer/generated";
+import { visit } from "unist-util-visit";
+
+function fixRelativeLinks() {
+	// biome-ignore lint/suspicious/noExplicitAny: TODO
+	return (tree: any) => {
+		const pattern = /(..\/)*public\//g;
+		visit(tree, "image", (node) => {
+			node.url = (node.url as string).replace(pattern, "/");
+		});
+		visit(tree, "html", (node) => {
+			node.value = (node.value as string).replace(pattern, "/");
+		});
+		visit(tree, "code", (node) => {
+			node.value = (node.value as string).replace(pattern, "/");
+		});
+
+		return tree;
+	};
+}
+
+const options = {
+	outputPath: (path) => {
+		if (path === LLMS_TXT_OUTPUT_DIR_INPUT) {
+			return "./public/llms.txt";
+		}
+		// path is "./content/posts/[slug].mdx"
+		const slug = path.split("/").slice(3).join("/");
+		const withoutExtension = slug.split(".").slice(0, -1).join(".");
+		return `./public/${withoutExtension}.md`;
+	},
+	formatFrontmatter: (frontmatter) => ({
+		title: frontmatter.title,
+		description: frontmatter.description,
+		datePublished: frontmatter.datePublished,
+	}),
+	sections: [
+		{
+			title: "Vahor.fr",
+		},
+		{
+			title: "Posts",
+			links: allDocuments.map((doc) => ({
+				title: doc.title,
+				url: `https://vahor.fr/${doc.pageType}/${doc.slug}.md`,
+				description: doc.description,
+			})),
+		},
+	],
+	content: allDocuments.map((doc) => ({
+		path: `./content/${doc._raw.sourceFilePath}`,
+	})),
+	remarkPlugins: [fixRelativeLinks],
+} satisfies PluginOptions;
+
+generate(options);
