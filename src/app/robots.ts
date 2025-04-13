2921427db96537b62363cf4f01cece9a3352bce1
Vahor <me@vahor.fr>
2025-04-13 17:25:55 +0200

    remove allow in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			userAgent: "*",
-			allow: "/",
+			disallow: "",
 		},
 		sitemap: `${BASE_URL}/sitemap.xml`,
 		host: BASE_URL,
