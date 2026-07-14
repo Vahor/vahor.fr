d543c8597011af3ebb8a44cfecdcbd6d7a65eb4a
Vahor <me@vahor.fr>
2026-07-14 15:55:20 +0200

    no join

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,15 +1,14 @@
 export function GET() {
 	return new Response(
-		[
-			"User-agent: *",
-			"Allow: /",
-			"",
-			"Sitemap: https://vahor.fr/sitemap.xml",
-			"Host: https://vahor.fr",
-			"",
-			"# Content Signals: https://contentsignals.org/",
-			"Content-Signal: ai-train=no, search=yes, ai-input=no",
-		].join("\n"),
+		`
+User-agent: *
+Allow: /
+
+Sitemap: https://vahor.fr/sitemap.xml
+Host: https://vahor.fr
+
+# Content Signals: https://contentsignals.org/
+Content-Signal: ai-train=no, search=yes, ai-input=no`,
 		{ headers: { "Content-Type": "text/plain" } },
 	);
 }
